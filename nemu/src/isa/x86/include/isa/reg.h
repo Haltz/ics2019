b2e0305bb8c4ef8e0e708e49d6d488dbc44aaea9
tracer-ics2019 <tracer@njuics.org>
2021-01-08 23:47:13 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:47:13 up 48 min,  1 user,  load average: 1.39, 1.11, 1.13 7e9ae7323f043a40933c0b7d2f4ce0b276b779e

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -2,6 +2,7 @@
 #define __X86_REG_H__
 
 #include "common.h"
+#include "mmu.h"
 
 #define PC_START IMAGE_START
 
@@ -16,21 +17,48 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+typedef struct { 
+  union{
+   union{	  
+     uint32_t _32;
+     uint16_t _16;
+     uint8_t _8[2];
+   } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+   struct{  
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+   };
+  };
   vaddr_t pc;
+  vaddr_t cs;
+
+  union {
+    struct{
+      uint32_t CF:1;
+      unsigned:5;
+      uint32_t ZF:1; 
+      uint32_t SF:1; 
+      unsigned :1; 
+      uint32_t IF:1; 
+      unsigned :1; 
+      uint32_t OF:1; 
+      unsigned : 20;
+    };
+    uint32_t val;
+  } eflags;
+  
+  struct{
+    uint32_t base;
+    uint32_t limit;
+  }idtr;
+  
+  CR0 cr0;
+  CR3 cr3;
 
 } CPU_state;
 
