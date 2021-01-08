1daa250422328c96b0e3efa542dcefcbb633e17b
tracer-ics2019 <tracer@njuics.org>
2021-01-09 00:36:57 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:36:57 up  1:38,  1 user,  load average: 0.65, 0.82, 0.94 b221f5ece6c113db351007227c2395c2192bed34

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -7,6 +7,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include "isa/reg.h"
+
 void cpu_exec(uint64_t);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
