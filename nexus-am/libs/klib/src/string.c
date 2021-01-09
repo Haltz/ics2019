182652a985c18c983a1e835f0a30e2b321a35094
tracer-ics2019 <tracer@njuics.org>
2021-01-10 02:20:06 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:20:06 up  2:24,  1 user,  load average: 0.68, 0.70, 0.65 e15bc087a2998461d30b9f653e6309ca32c96897

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,62 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+	if (s == NULL)
+		return 0;
+
+	size_t len = 0;
+	while ((*s++) != '\0')
+		len++;
+
+	return len;
 }
 
-char *strcpy(char* dst,const char* src) {
-  return NULL;
+char *strcpy(char *dst, const char *src) {
+	if (dst == NULL || src == NULL)
+		return dst;
+
+	char *t = dst;
+	while ((*t++ = *src++) != '\0')
+		;
+
+	return dst;
 }
 
-char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+char *strncpy(char *dst, const char *src, size_t n) {
+	if (dst == NULL || src == NULL)
+		return dst;
+
+	char *t = dst;
+	size_t i = 0;
+	for (i = 0; i < n && (*src != '\0'); i++) {
+		*t++ = *src++;
+	}
+
+	return dst;
 }
 
-char* strcat(char* dst, const char* src) {
-  return NULL;
+char *strcat(char *dst, const char *src) {
+	return NULL;
 }
 
-int strcmp(const char* s1, const char* s2) {
-  return 0;
+int strcmp(const char *s1, const char *s2) {
+	return 0;
 }
 
-int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+int strncmp(const char *s1, const char *s2, size_t n) {
+	return 0;
 }
 
-void* memset(void* v,int c,size_t n) {
-  return NULL;
+void *memset(void *v, int c, size_t n) {
+	return NULL;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+void *memcpy(void *out, const void *in, size_t n) {
+	return NULL;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+int memcmp(const void *s1, const void *s2, size_t n) {
+	return 0;
 }
 
 #endif
