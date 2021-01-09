09501365d4eeb56ed128bb7d107b3f0292e1ea2a
tracer-ics2019 <tracer@njuics.org>
2021-01-10 01:55:58 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:55:58 up  2:00,  1 user,  load average: 1.03, 0.76, 0.67 d1babe5ff9b9b66eb9eaeb6cd75a786f87aff31

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -40,14 +40,26 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-	TODO();
+	rtl_addi(&s0, &id_dest->val, 1);
+	operand_write(id_dest, &s0);
 
+	rtl_xor(&s1, &id_dest->val, &s0);
+	rtl_msb(&s1, &s1, id_dest->width);
+	rtl_set_OF(&s1);
+
+	rtl_update_ZFSF(&s0, id_dest->width);
 	print_asm_template1(inc);
 }
 
 make_EHelper(dec) {
-	TODO();
+	rtl_subi(&s0, &id_dest->val, 1);
+	operand_write(id_dest, &s0);
 
+	rtl_xor(&s1, &id_dest->val, &s0); //check
+	rtl_msb(&s1, &s1, id_dest->width);
+	rtl_set_OF(&s1);
+
+	rtl_update_ZFSF(&s0, id_dest->width);
 	print_asm_template1(dec);
 }
 
