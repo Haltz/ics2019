7126cf0a2d9651fbfa4795b4bc208067079e0f89
tracer-ics2019 <tracer@njuics.org>
2021-01-10 02:11:44 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:11:44 up  2:15,  1 user,  load average: 0.48, 0.78, 0.71 b30ab99f4c15c435b44f1e9dbae99b3815dc39a1

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,19 +4,116 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+	char buff[1024];
+	va_list args;
+	int n;
+
+	va_start(args, fmt);
+	n = vsprintf(buff, fmt, args);
+	int i = 0;
+	while (buff[i] != '\0') {
+		_putc(buff[i]);
+		i++;
+	}
+	//_putc('p');
+	va_end(args);
+	return n;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+	int length;
+	const char *t;
+	char *str = out;
+	for (; *fmt; fmt++) {
+		if (*fmt != '%') {
+			*str++ = *fmt;
+			continue;
+		}
+		fmt++;
+		switch (*fmt) {
+		case 's': {
+			t = va_arg(ap, char *);
+			length = strlen(t);
+			for (int i = 0; i < length; i++) {
+				*str++ = *t++;
+			}
+			break;
+		}
+		case 'd': {
+			int num = va_arg(ap, int);
+			int i = 0;
+			char c;
+			if (num == 0)
+				*str++ = '0';
+			else {
+				if (num < 0) {
+					*str++ = '-';
+					num = (-num);
+				}
+				while (num != 0) {
+					*str++ = num % 10 + '0';
+					i++;
+					num /= 10;
+				}
+			}
+			for (int j = 1; j <= (i / 2); j++) {
+				c = *(str - j);
+				*(str - j) = *(str - (i - j + 1));
+				*(str - (i - j + 1)) = c;
+			}
+			break;
+		}
+		case 'c': {
+			int num = va_arg(ap, int);
+			*str++ = (char)num;
+			break;
+		}
+		case 'p':
+		case 'x': {
+			uint32_t num = va_arg(ap, uint32_t);
+			int i = 0;
+			char nums[20];
+			if (num == 0)
+				nums[0] = '0';
+			else {
+				while (num != 0) {
+					uint32_t x = num % 16;
+					if (x < 10)
+						nums[i++] = x + '0';
+					else
+						nums[i++] = x - 10 + 'a';
+					num /= 16;
+				}
+			}
+			*str++ = '0';
+			*str++ = 'x';
+			for (int j = i - 1; j >= 0; j--) {
+				*str++ = nums[j];
+			}
+			break;
+		}
+
+		default:
+			break;
+		}
+	}
+	*str = '\0';
+	return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+	va_list args;
+	int n;
+
+	va_start(args, fmt);
+	n = vsprintf(out, fmt, args);
+	va_end(args);
+
+	return n;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+	return 0;
 }
 
 #endif
