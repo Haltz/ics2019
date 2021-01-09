5d10c1ca35e1fb146e573ebf8598e6d6d7e23358
tracer-ics2019 <tracer@njuics.org>
2021-01-09 13:57:26 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:57:26 up 14:59,  1 user,  load average: 0.50, 0.32, 0.41 ec2a39a8f86b878992b2e7e62c7ff045ee857ce8

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,74 +17,79 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct { 
-  union{
-   union{	  
-     uint32_t _32;
-     uint16_t _16;
-     uint8_t _8[2];
-   } gpr[8];
+typedef struct {
+	union {
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
-  /* Do NOT change the order of the GPRs' definitions. */
+		/* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+		/* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-   struct{  
-    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-   };
-  };
-  vaddr_t pc;
-  vaddr_t cs;
+		struct {
+			rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+	};
+	vaddr_t pc;
+	vaddr_t cs;
 
-  union {
-    struct{
-      uint32_t CF:1;
-      unsigned:5;
-      uint32_t ZF:1; 
-      uint32_t SF:1; 
-      unsigned :1; 
-      uint32_t IF:1; 
-      unsigned :1; 
-      uint32_t OF:1; 
-      unsigned : 20; // reserved bits, to align
-    };
-    uint32_t val;
-  } eflags;
-  
-  struct{
-    uint32_t base;
-    uint32_t limit;
-  }idtr;
-  
-  CR0 cr0;
-  CR3 cr3;
+	union {
+		struct {
+			uint32_t CF : 1;
+			unsigned : 5;
+			uint32_t ZF : 1;
+			uint32_t SF : 1;
+			unsigned : 1;
+			uint32_t IF : 1;
+			unsigned : 1;
+			uint32_t OF : 1;
+			unsigned : 20; // reserved bits, to align
+		};
+		uint32_t val;
+	} eflags;
+
+	struct {
+		uint32_t base;
+		uint32_t limit;
+	} idtr;
+
+	CR0 cr0;
+	CR3 cr3;
 
 } CPU_state;
 
 static inline int check_reg_index(int index) {
-  assert(index >= 0 && index < 8);
-  return index;
+	assert(index >= 0 && index < 8);
+	return index;
 }
 
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
 
-static inline const char* reg_name(int index, int width) {
-  extern const char* regsl[];
-  extern const char* regsw[];
-  extern const char* regsb[];
-  assert(index >= 0 && index < 8);
+static inline const char *reg_name(int index, int width) {
+	extern const char *regsl[];
+	extern const char *regsw[];
+	extern const char *regsb[];
+	assert(index >= 0 && index < 8);
 
-  switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
-  }
+	switch (width) {
+	case 4:
+		return regsl[index];
+	case 1:
+		return regsb[index];
+	case 2:
+		return regsw[index];
+	default:
+		assert(0);
+	}
 }
 
 void isa_reg_display(void);
+uint32_t isa_reg_str2val(const char *s, bool *success);
 
 #endif
