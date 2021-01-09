4b9e72b0d992de772e027151ca2e2574ed6dfd9e
tracer-ics2019 <tracer@njuics.org>
2021-01-09 21:11:53 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:11:53 up  4:44,  1 user,  load average: 0.45, 0.59, 0.46 7db97c9f126c9a0941c5319d1cb05a3658ce7407

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -51,7 +51,7 @@ make_group(gp1, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY)
 		/* 0x28 */ IDEXW(G2E, sub, 1), IDEX(G2E, sub), IDEXW(E2G, sub, 1), IDEX(E2G, sub),
 		/* 0x2c */ IDEXW(I2a, sub, 1), IDEX(I2a, sub), EMPTY, EMPTY,
 
-		/* 0x30 */ EMPTY, EMPTY, EMPTY, EMPTY,
+		/* 0x30 */ EMPTY, IDEX(G2E, xor), EMPTY, IDEX(E2G, xor),
 		/* 0x34 */ EMPTY, EMPTY, EMPTY, EMPTY,
 		/* 0x38 */ EMPTY, EMPTY, EMPTY, EMPTY,
 		/* 0x3c */ EMPTY, EMPTY, EMPTY, EMPTY,
