2adf6169e606cf8e823d3ecd12f5212c6818d691
tracer-ics2019 <tracer@njuics.org>
2021-01-09 21:01:02 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.8.0-34-generic #37~20.04.2-Ubuntu SMP Thu Dec 17 14:53:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:01:02 up  4:33,  1 user,  load average: 0.39, 0.53, 0.30 5f88b11da5334f73185de44f69b83fc2d5e5d

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -10,7 +10,6 @@ make_EHelper(jmp) {
 }
 
 make_EHelper(jcc) {
-	// the target address is calculated at the decode stage
 	uint32_t cc = decinfo.opcode & 0xf;
 	rtl_setcc(&s0, cc);
 	rtl_li(&s1, 0);
