11c624b927bb1d37ed38ee2f72d547c85a067d51
tracer-ics2019 <tracer@njuics.org>
2021-01-10 02:04:47 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:04:47 up  2:08,  1 user,  load average: 0.80, 0.63, 0.61 cdaaa93b3089d4484254d9d44e116e4363674411

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -53,9 +53,11 @@ make_EHelper(leave) {
 
 make_EHelper(cltd) {
 	if (decinfo.isa.is_operand_size_16) {
-		TODO();
+		rtl_sext(&s0, &reg_l(R_EAX), 2);
+		rtl_sari(&s0, &s0, 15);
+		rtl_sr(R_DX, &s0, 2);
 	} else {
-		TODO();
+		rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
 	}
 
 	print_asm(decinfo.isa.is_operand_size_16 ? "cwtl" : "cltd");
@@ -63,9 +65,11 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
 	if (decinfo.isa.is_operand_size_16) {
-		TODO();
+		rtl_lr(&s0, R_AX, 1);
+		rtl_sext(&s0, &s0, 1);
+		rtl_sr(R_AX, &s0, 1);
 	} else {
-		TODO();
+		rtl_sext(&reg_l(R_EAX), &reg_l(R_EAX), 2);
 	}
 
 	print_asm(decinfo.isa.is_operand_size_16 ? "cbtw" : "cwtl");
