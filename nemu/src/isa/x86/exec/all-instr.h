70b59f7141b79c0be28bfeacb63d8542f94ba702
tracer-ics2019 <tracer@njuics.org>
2021-01-10 01:53:27 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:53:27 up  1:57,  1 user,  load average: 0.65, 0.70, 0.65 d3ec2d9675f0795255b10333ec4f3d4426456958

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -47,8 +47,6 @@ make_EHelper(nop);
 make_EHelper(setcc);
 make_EHelper(mov);
 make_EHelper(movzx);
-make_EHelper(movsb);
-make_EHelper(movs);
 make_EHelper(movsx);
 make_EHelper(mov_r2cr);
 make_EHelper(mov_cr2r);
