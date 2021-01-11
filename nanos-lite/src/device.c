9e41c24f08ed01e60390cfb0acef2aee926f8f97
tracer-ics2019 <tracer@njuics.org>
2021-01-11 22:14:09 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:14:09 up 19:03,  1 user,  load average: 1.38, 1.11, 0.79 2e5a2d6ddd5dc3642e11cbb3650bdedc38b5de8f

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,8 +1,15 @@
 #include "common.h"
 #include <amdev.h>
 
+#ifndef KEYDOWN_MASK
+#define KEYDOWN_MASK 0x8000
+#endif
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-	return 0;
+	for (int i = 0; i < len; i++) {
+		_putc(((char *)buf)[i]);
+	}
+	return len;
 }
 
 #define NAME(key) [_KEY_##key] = #key,
@@ -10,26 +17,49 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 static const char *keyname[256] __attribute__((used)) = { [_KEY_NONE] = "NONE", _KEYS(NAME) };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-	return 0;
+	int  key  = read_key();
+	bool down = false;
+	if (key & KEYDOWN_MASK) {
+		key ^= KEYDOWN_MASK;
+		down = true;
+	}
+	if (key != _KEY_NONE) {
+		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+
+	} else {
+		int time = uptime();
+		sprintf(buf, "t %d\n", time);
+	}
+	return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-	return 0;
+	if (len + offset > strlen(dispinfo))
+		len = strlen(dispinfo) - offset;
+	strncpy(buf, &dispinfo[offset], len);
+	return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-	return 0;
+	offset /= 4;
+	int x = offset % screen_width();
+	int y = offset / screen_width();
+	draw_rect((uint32_t *)buf, x, y, len / 4, 1);
+	return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-	return 0;
+	draw_sync();
+	return len;
 }
 
 void init_device() {
 	Log("Initializing devices...");
 	_ioe_init();
 
+	// print the string to array `dispinfo` with the format
+	// described in the Navy-apps convention
 	sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
