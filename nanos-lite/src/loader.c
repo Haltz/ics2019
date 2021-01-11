793f0505d4587291ddff0e4b3efc3ae352832696
tracer-ics2019 <tracer@njuics.org>
2021-01-12 05:07:08 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:07:08 up  2:03,  1 user,  load average: 0.39, 0.16, 0.12 e65d7cc37aae91ba7eb6475c75ca9ad5a6353ffe

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,29 +11,25 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-	Elf_Ehdr elf_header;
-	// READ ELF HEADER
-	ramdisk_read((void *)&elf_header, 0, sizeof(Elf_Ehdr));
-	// Log("???");
+	int	 fd = fs_open(filename, 0, 0);
+	Elf_Ehdr ehdr;
+	fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
 
-	// define porgram header array to read program headers
-	Elf_Phdr program_header[128];
-	ramdisk_read((void *)program_header, elf_header.e_phoff, elf_header.e_phentsize * elf_header.e_phnum);
-	// Log("???");
+	for (int i = 0; i < ehdr.e_phnum; i++) {
+		Elf_Phdr phdr;
+		fs_lseek(fd, ehdr.e_phoff + ehdr.e_phentsize * i, SEEK_SET);
+		fs_read(fd, &phdr, ehdr.e_phentsize);
 
-	// using the program header to load segment
-	// such as data segment
-	for (int i = 0; i < elf_header.e_phnum; i++) {
-		// load type must be PT_LOAD
-		if (program_header[i].p_type == PT_LOAD) {
-			// copy the ramdisk's sengment to the NEMU's memory
-			ramdisk_read((void *)program_header[i].p_vaddr, program_header[i].p_offset, program_header[i].p_filesz);
+		if (phdr.p_type == PT_LOAD) {
+			fs_lseek(fd, phdr.p_offset, SEEK_SET);
 
-			// assgin the uninitialized data segment to 0
-			memset((void *)(program_header[i].p_vaddr + program_header[i].p_filesz), 0, program_header[i].p_memsz - program_header[i].p_filesz);
+			fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+			memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
 		}
 	}
-	return elf_header.e_entry;
+
+	fs_close(fd);
+	return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
