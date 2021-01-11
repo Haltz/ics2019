11c530b6ff27a6e38db4b2861c785ead9188deb7
tracer-ics2019 <tracer@njuics.org>
2021-01-11 16:58:14 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:58:14 up 13:47,  1 user,  load average: 0.55, 0.60, 0.45 57e80a3eea070a74b5b2836a5a01b565eed57ef2

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -10,6 +10,9 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
 	GateDesc gatedesc;
 	gatedesc.offset_15_0 = (vaddr_read(cpu.idtr.base + NO * 8, 4) & 0x0000ffff);
 	gatedesc.offset_31_16 = ((vaddr_read(cpu.idtr.base + NO * 8 + 4, 4) & 0xffff0000) >> 16);
+
+	vaddr_t goalAddr = gatedesc.offset_15_0 | (gatedesc.offset_31_16 & 0x0000ffff) << 16;
+	rtl_j(goalAddr);
 }
 
 bool isa_query_intr(void) {
