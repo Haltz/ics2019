44c5c49be40b354f87de9bd7ce2c74441a804ecb
tracer-ics2019 <tracer@njuics.org>
2021-01-12 07:03:14 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:03:14 up  4:00,  1 user,  load average: 0.88, 1.24, 1.07 bb998a119825820f3d9a31445adf15bc58707121

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -8,8 +8,10 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 	switch (reg) {
 	case _DEVREG_TIMER_UPTIME: {
 		_DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-		uptime->hi		    = 0;
-		uptime->lo		    = 0;
+
+		uint64_t time = inl(RTC_ADDR) - time;
+		uptime->hi    = (uint32_t)(time >> 32);
+		uptime->lo    = (uint32_t)(time & 0xffffffff);
 		return sizeof(_DEV_TIMER_UPTIME_t);
 	}
 	case _DEVREG_TIMER_DATE: {
