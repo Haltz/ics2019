bd90b336324541cf5e213d4e29f7efdf3637f779
tracer-ics2019 <tracer@njuics.org>
2021-01-12 05:43:36 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:43:36 up  2:40,  1 user,  load average: 0.69, 0.78, 0.70 8060cc5c3cdfc554e224ec3843e47f334cbb6b4b

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -55,12 +55,11 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-	_exit(SYS_open);
-	return 0;
+	return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count) {
-	return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
+	return _syscall_(SYS_write, fd, buf, count);
 }
 
 extern char _end;
