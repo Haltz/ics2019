d0b38f96dc75fa288cca9dfa9417cacee3200ab3
tracer-ics2019 <tracer@njuics.org>
2021-01-12 01:02:56 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:02:56 up 21:52,  1 user,  load average: 0.99, 0.77, 0.76 724174e7b638a75a201b498f4298b08461d3ecec

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,17 +3,26 @@
 
 _Context *do_syscall(_Context *c) {
 	uintptr_t a[4];
-	a[0] = c->GPR1;
+	a[0]	= c->GPR1;
+	a[1]	= c->GPR2;
+	a[2]	= c->GPR3;
+	a[3]	= c->GPR4;
+	int res = -1;
 
 	switch (a[0]) {
-	case SYS_yield:
-		c->GPRx = 0;
-		_yield();
+	case SYS_exit:
+		_halt(a[1]);
 		break;
 
+	case SYS_yield:
+		res = 0;
+		_yield();
+		break;
 	default:
 		panic("Unhandled syscall ID = %d", a[0]);
 	}
+
+	c->GPRx = res;
 
 	return NULL;
 }
