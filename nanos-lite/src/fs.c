57e9a078f3d652b332452124a05a921accd59de2
tracer-ics2019 <tracer@njuics.org>
2021-01-12 00:06:19 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:06:19 up 20:55,  1 user,  load average: 1.09, 0.74, 0.52 8d76c501deef71abefab8d9cbccb67eeed5fb25b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -31,9 +31,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-	{ "stdin", 0, 0, 0, invalid_read, invalid_write },	{ "stdout", 0, 0, 0, invalid_read, serial_write },	{ "stderr", 0, 0, 0, invalid_read, serial_write },   { "/dev/fb", 0, 0, 0, invalid_read, fb_write },
-	{ "/dev/events", 0, 0, 0, events_read, invalid_write }, { "/dev/fbsync", 0, 0, 0, invalid_read, fbsync_write }, { "/dev/tty", 0, 0, 0, invalid_read, serial_write }, { "/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write },
-#include "files.h"
+	{ "stdin", 0, 0, 0, invalid_read, invalid_write },
+	{ "stdout", 0, 0, 0, invalid_read, serial_write },
+	{ "stderr", 0, 0, 0, invalid_read, serial_write },
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
@@ -41,84 +41,4 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
 	// initialize the size of /dev/fb
 	file_table[FD_FB].size = (screen_width() * screen_height()) << 2;
-}
-
-int fs_open(const char *pathname, int flags, int mode) {
-	for (int i = 0; i < NR_FILES; i++) {
-		if (strcmp(file_table[i].name, pathname) == 0) {
-			file_table[i].open_offset = 0;
-			return i;
-		}
-	}
-	printf("file %s does not exist!", pathname);
-	assert(0);
-	return -1;
-}
-
-size_t fs_read(int fd, void *buf, size_t len) {
-	assert(fd >= 0 && fd < NR_FILES);
-
-	int r_len = len;
-	if (file_table[fd].size > 0 && file_table[fd].open_offset + len > file_table[fd].size) {
-		r_len = file_table[fd].size - file_table[fd].open_offset;
-	}
-	assert(r_len >= 0);
-
-	size_t length = 0;
-	if (file_table[fd].read == NULL) {
-		length = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, r_len);
-	} else {
-		length = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, r_len);
-	}
-	file_table[fd].open_offset += length;
-	return length;
-}
-
-int fs_close(int fd) {
-	return 0;
-}
-
-size_t fs_write(int fd, const void *buf, size_t len) {
-	assert(fd >= 0 && fd < NR_FILES);
-
-	int w_len = len;
-	if (file_table[fd].size > 0 && file_table[fd].open_offset + len > file_table[fd].size) {
-		w_len = file_table[fd].size - file_table[fd].open_offset;
-	}
-
-	assert(w_len >= 0);
-
-	size_t length = 0;
-	if (file_table[fd].write == NULL) {
-		length = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, w_len);
-	} else {
-		length = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, w_len);
-	}
-
-	file_table[fd].open_offset += length;
-	return length;
-}
-
-size_t fs_lseek(int fd, size_t offset, int whence) {
-	assert(fd >= 0 && fd < NR_FILES);
-	size_t open_offset = file_table[fd].open_offset;
-	switch (whence) {
-	case SEEK_SET:
-		open_offset = offset;
-		break;
-	case SEEK_CUR:
-		open_offset += offset;
-		break;
-	case SEEK_END:
-		open_offset = file_table[fd].size + offset;
-		break;
-	default:
-		panic("There is no such whence");
-	}
-	file_table[fd].open_offset = open_offset;
-	return open_offset;
-}
-
-size_t fs_open_offset(int fd) {
-	return file_table[fd].open_offset;
 }
