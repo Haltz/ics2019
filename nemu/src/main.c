112fccc699d53be7744b2371eb1726ad921830c9
tracer-ics2019 <tracer@njuics.org>
2021-01-12 00:17:10 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:17:10 up 21:06,  1 user,  load average: 0.01, 0.13, 0.28 54866773281b66c01623a66827f802acfa27ecb1

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,9 +1,11 @@
-int init_monitor(int, char *[]);
+int  init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
 	/* Initialize the monitor. */
 	int is_batch_mode = init_monitor(argc, argv);
+
+	printf("123\n");
 
 	/* Receive commands from user. */
 	ui_mainloop(is_batch_mode);
