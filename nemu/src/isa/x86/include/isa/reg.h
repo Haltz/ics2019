304488ecdfc085d9c6dce0018b33d1618edbdab4
tracer-ics2019 <tracer@njuics.org>
2021-01-11 23:00:26 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:00:26 up 19:49,  1 user,  load average: 0.25, 0.50, 0.57 3405f3bb5802f60ce419afb83b0b3de02b7591df

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -10,7 +10,7 @@ enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
-/* TODO: Re-organize the `CPU_state' structure to match the register
+/* Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
  * access cpu.gpr[3]._16, we will get the `bx' register; if we access
  * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
@@ -22,7 +22,7 @@ typedef struct {
 		union {
 			uint32_t _32;
 			uint16_t _16;
-			uint8_t _8[2];
+			uint8_t	 _8[2];
 		} gpr[8];
 
 		/* Do NOT change the order of the GPRs' definitions. */
@@ -90,7 +90,7 @@ static inline const char *reg_name(int index, int width) {
 	}
 }
 
-void isa_reg_display(void);
+void	 isa_reg_display(void);
 uint32_t isa_reg_str2val(const char *s, bool *success);
 
 #endif
