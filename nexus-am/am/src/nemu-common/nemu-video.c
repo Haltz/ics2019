dbd8a03050e8e814e34bde1fda49dc946bde5f63
tracer-ics2019 <tracer@njuics.org>
2021-01-12 06:56:50 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:56:50 up  3:53,  1 user,  load average: 0.91, 0.92, 0.85 fa6aab58f40f714e113a3f277e94d51c916fdedf

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -3,30 +3,36 @@
 #include <nemu.h>
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_INFO: {
-      _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
-      return sizeof(_DEV_VIDEO_INFO_t);
-    }
-  }
-  return 0;
+	switch (reg) {
+	case _DEVREG_VIDEO_INFO: {
+		_DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
+		info->width		= 0;
+		info->height		= 0;
+		return sizeof(_DEV_VIDEO_INFO_t);
+	}
+	}
+	return 0;
 }
 
 size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+	switch (reg) {
+	case _DEVREG_VIDEO_FBCTL: {
+		_DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
 
-      if (ctl->sync) {
-        outl(SYNC_ADDR, 0);
-      }
-      return size;
-    }
-  }
-  return 0;
+		if (ctl->sync) {
+			outl(SYNC_ADDR, 0);
+		}
+		return size;
+	}
+	}
+	return 0;
 }
 
 void __am_vga_init() {
+	int	  i;
+	int	  size = screen_width() * screen_height();
+	uint32_t *fb   = (uint32_t *)(uintptr_t)FB_ADDR;
+	for (i = 0; i < size; i++)
+		fb[i] = i;
+	draw_sync();
 }
