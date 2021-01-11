8141e7a831e97feb9140990cf280e9f9f97e443b
tracer-ics2019 <tracer@njuics.org>
2021-01-11 21:57:55 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:57:55 up 18:47,  1 user,  load average: 1.71, 0.67, 0.29 8abc4af0334558b326163b05f66cc37241a7152

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,26 +11,7 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-	Elf_Ehdr elfheader;
-	Elf_Phdr programheader;
-	int fd = fs_open(filename, 0, 0);
-	assert(fd != -1);
-	fs_read(fd, &elfheader, sizeof(Elf_Ehdr));
-	fs_lseek(fd, elfheader.e_phoff, SEEK_SET);
-	size_t open_offset;
-
-	for (uint16_t i = 0; i < elfheader.e_phnum; i++) {
-		fs_read(fd, &programheader, sizeof(Elf_Phdr));
-		open_offset = fs_open_offset(fd);
-		if (programheader.p_type == PT_LOAD) {
-			fs_lseek(fd, programheader.p_offset, SEEK_SET);
-			fs_read(fd, (void *)programheader.p_vaddr, programheader.p_filesz);
-			memset((void *)(programheader.p_vaddr + programheader.p_filesz), 0, (programheader.p_memsz - programheader.p_filesz));
-		}
-		fs_lseek(fd, open_offset, SEEK_SET);
-	}
-	fs_close(fd);
-	return elfheader.e_entry;
+	return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
@@ -42,7 +23,7 @@ void naive_uload(PCB *pcb, const char *filename) {
 void context_kload(PCB *pcb, void *entry) {
 	_Area stack;
 	stack.start = pcb->stack;
-	stack.end = stack.start + sizeof(pcb->stack);
+	stack.end   = stack.start + sizeof(pcb->stack);
 
 	pcb->cp = _kcontext(stack, entry, NULL);
 }
@@ -52,7 +33,7 @@ void context_uload(PCB *pcb, const char *filename) {
 
 	_Area stack;
 	stack.start = pcb->stack;
-	stack.end = stack.start + sizeof(pcb->stack);
+	stack.end   = stack.start + sizeof(pcb->stack);
 
 	pcb->cp = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
 }
