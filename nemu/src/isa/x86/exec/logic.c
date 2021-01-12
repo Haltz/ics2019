e4c83a4c909ba6a8e2285643f8e6756ea80785ed
tracer-ics2019 <tracer@njuics.org>
2021-01-12 19:43:45 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:43:45 up 16:40,  1 user,  load average: 1.61, 0.66, 0.44 7e5645781409544df6577b07d2164c18c6c72a69

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -25,12 +25,15 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-	rtl_xor(&id_dest->val, &id_dest->val, &id_src->val);
-	operand_write(id_dest, &id_dest->val);
-	rtl_li(&ir, 0);
-	rtl_set_CF(&ir);
-	rtl_set_OF(&ir);
-	rtl_update_ZFSF(&id_dest->val, id_dest->width);
+	rtl_xor(&s0, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &s0);
+	rtl_update_ZFSF(&s0, id_dest->width);
+
+	// CF = 0, OF = 0
+	s1 = 0;
+	rtl_set_CF(&s1);
+	rtl_set_OF(&s1);
+
 	print_asm_template2(xor);
 }
 
