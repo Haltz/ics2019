fb3c76491233516fe621e6e3901bed6a2086aeda
tracer-ics2019 <tracer@njuics.org>
2021-01-12 08:12:24 +0800

    >  compile U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:12:24 up  5:09,  1 user,  load average: 1.09, 0.92, 0.74 cc64884c5da2e38424b00fe6813d6458ae749307

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,8 +2,25 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+	if (ref_r->eax != reg_l(R_EAX) || ref_r->ebx != reg_l(R_EBX) || ref_r->ecx != reg_l(R_ECX) || ref_r->edx != reg_l(R_EDX) || ref_r->ebp != reg_l(R_EBP) || ref_r->esi != reg_l(R_ESI) || ref_r->edi != reg_l(R_EDI) || ref_r->pc != cpu.pc) {
+		return false;
+	}
+	return true;
 }
 
 void isa_difftest_attach(void) {
+	ref_difftest_memcpy_from_dut(0, &pmem[0], 0x7c00);
+	ref_difftest_memcpy_from_dut(0x100000, &pmem[0x100000], PMEM_SIZE - 0x100000);
+	ref_difftest_setregs(&cpu);
+
+	ref_difftest_memcpy_from_dut(0x7e00, (void *)&cpu.idtr.limit, sizeof(cpu.idtr.limit));
+	ref_difftest_memcpy_from_dut(0x7e00 + sizeof(cpu.idtr.limit), (void *)&cpu.idtr.base, sizeof(cpu.idtr.base));
+
+	uint8_t lidt_code[] = { 0x0f, 0x01, 0x1d, 0x00, 0x7e, 0x00, 0x00 };
+	ref_difftest_memcpy_from_dut(0x7e40, (void *)&lidt_code, sizeof(lidt_code));
+	CPU_state cpu_t = cpu;
+	cpu_t.pc	= 0x7e40;
+	ref_difftest_setregs(&cpu_t);
+	ref_difftest_exec(1);
+	ref_difftest_setregs(&cpu);
 }
