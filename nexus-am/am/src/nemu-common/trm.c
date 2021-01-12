ca4c7f6044bbf4ff4fec721bb5763efe0c2372ec
tracer-ics2019 <tracer@njuics.org>
2021-01-12 19:00:54 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:00:54 up 15:57,  1 user,  load average: 0.48, 0.53, 0.54 e7477cbedb8ab690f2060fd1b66b25238fc4b255

--- nexus-am/am/src/nemu-common/trm.c
+++ nexus-am/am/src/nemu-common/trm.c
@@ -3,27 +3,29 @@
 
 extern char _heap_start;
 extern char _heap_end;
-int main(const char *args);
+int	    main(const char *args);
 
 _Area _heap = {
-  .start = &_heap_start,
-  .end = &_heap_end,
+	.start = &_heap_start,
+	.end   = &_heap_end,
 };
 
 void _putc(char ch) {
-  outb(SERIAL_PORT, ch);
+	outb(SERIAL_PORT, ch);
 }
 
 void _halt(int code) {
-  nemu_trap(code);
+	nemu_trap(code);
 
-  // should not reach here
-  while (1);
+	// should not reach here
+	while (1)
+		;
 }
 
 void _trm_init() {
-  extern const char _start;
-  const char *mainargs = &_start - 0x100000;
-  int ret = main(mainargs);
-  _halt(ret);
+	extern const char _start;
+	const char *	  mainargs = &_start - 0x100000;
+	printf("1\n");
+	int ret = main(mainargs);
+	_halt(ret);
 }
