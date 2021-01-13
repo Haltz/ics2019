a158ce1d64d05b4a798d3caf6969cd6bd5be185c
tracer-ics2019 <tracer@njuics.org>
2021-01-13 23:17:24 +0800

    >  run U201714718 haltz Linux haltz-ubuntu 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:17:24 up  4:24,  2 users,  load average: 1.73, 2.02, 1.49 a806519dd3067d34e392ce147c2eeb1e71c64522

--- nexus-am/apps/litenes/src/fce.c
+++ nexus-am/apps/litenes/src/fce.c
@@ -148,12 +148,16 @@ void fce_update_screen() {
 
 int main(const char *rom_name) {
 	_ioe_init();
+	char *name = rom_name;
+	if (!rom_name) {
+		name = "";
+	}
 
 	struct rom *rom = &roms[0];
 	for (int i = 1; i < nroms; i++) {
 		struct rom *cur = &roms[i];
 		// if (strcmp(cur->name, "bomberman") == 0) {
-		if (strcmp(cur->name, rom_name || "NULL") == 0) {
+		if (strcmp(cur->name, name) == 0) {
 			rom = cur;
 		}
 	}
